21ac9bf408968f15bc12769015c5a444018eb7db
JimmyGarciaf <jimmygarcia470@gmail.com>
2025-10-17 13:25:58 -0600

    Commit 17-10-2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TasksListComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'To-DO_List';
+
 }

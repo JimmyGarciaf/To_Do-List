30846722f8d31c5d6600560c0d3d5f40d00fc867
JimmyGarciaf <jimmygarcia470@gmail.com>
2025-10-22 09:11:40 -0600

    Commit 22-10-2025

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-task-item',
@@ -8,5 +15,18 @@ import { Component } from '@angular/core';
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
+  // Recibe los datos de la tarea desde el componente padre
+  @Input() task!: Task;
 
+  // Emite un evento cuando la tarea se marca/desmarca o se elimina la tarea
+  @Output() toggle = new EventEmitter<number>();
+  @Output() delete = new EventEmitter<number>();
+
+  onToggle() {
+    this.toggle.emit(this.task.id);
+  }
+
+  onDelete() {
+    this.delete.emit(this.task.id);
+  }
 }

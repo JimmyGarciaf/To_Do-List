19bdeaadc587e7444ac6fd8a3debf11480607dd2
JimmyGarciaf <jimmygarcia470@gmail.com>
2025-10-30 16:36:35 -0600

    User-change-css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { TasksListComponent } from './tasks-list/tasks-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TasksListComponent, RouterOutlet],
+  imports: [TasksListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

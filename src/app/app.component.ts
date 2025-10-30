881e5716bad2e7ec8f8cbb4383339e8b73cdc6e1
JimmyGarciaf <jimmygarcia470@gmail.com>
2025-10-30 15:42:13 -0600

    Commit search and input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
 
@@ -10,5 +10,45 @@ import { TasksListComponent } from './tasks-list/tasks-list.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
+  private readonly PLACEHOLDER_FOCUS = 'Buscar';
+  private readonly PLACEHOLDER_BLUR = ' '; // Espacio invisible
+  
+  // --- Signals para el estado
+  isFocused = signal(false);
+  isHovered = signal(false);
+  searchValue = signal('');
 
+  // Computed Signal para determinar si el área de búsqueda está activa (foco o hover)
+  isActive = computed(() => this.isFocused() || this.isHovered());
+
+  // --- Computed Signal para el Placeholder
+  currentPlaceholder = computed(() => {
+    // Si el input está vacío Y tiene el foco, muestra 'Buscar'
+    if (this.isFocused() && this.searchValue().length === 0) {
+      return this.PLACEHOLDER_FOCUS;
+    }
+    // Si el input está vacío Y NO tiene el foco, muestra el espacio (invisible)
+    if (!this.isFocused() && this.searchValue().length === 0) {
+      return this.PLACEHOLDER_BLUR;
+    }
+    // Si hay valor, el navegador se encarga de ocultar el placeholder.
+    return ''; 
+  });
+
+  // --- Manejadores de Eventos
+  
+  handleFocus() {
+    this.isFocused.set(true);
+  }
+
+  handleBlur(event: Event) {
+    this.isFocused.set(false);
+  }
+  
+  handleInput(event: Event) {
+      // Actualiza la señal de valor de búsqueda en cada tipeo
+      const target = event.target as HTMLInputElement;
+      this.searchValue.set(target.value);
+  }
 }
